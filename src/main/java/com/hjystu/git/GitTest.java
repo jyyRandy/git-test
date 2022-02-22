3f4e3b6d6b6615d43f3d06f768ab740c47364013
Randy <89240300+jyyRandy@users.noreply.github.com>
2022-02-22 18:48:56 +0800

    pull commit

--- src/main/java/com/hjystu/git/GitTest.java
+++ src/main/java/com/hjystu/git/GitTest.java
@@ -9,5 +9,6 @@ public class GitTest {
         System.out.println("master");
         System.out.println("hello git!");
         System.out.println("push test!");
+        System.out.println("pull test!");
     }
 }
